5200569cdb4a228fc6a4a894f89e8f3ae3a9bffd
jonboh <jon.bosque.hernando@gmail.com>
2024-05-23 13:58:48 +0200

    release version 0.1.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="DBconnector",
-    version="0.0.0",
+    version="0.1.0",
     packages=find_packages(),
     description="Utility to connect to the neware server and fetch the data",
     long_description=open("README.md").read(),
